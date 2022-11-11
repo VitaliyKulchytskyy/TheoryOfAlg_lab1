8cd52de964777fc48692d2bb2c287e41eded84f2
Karlenko <vitamin6754@gmail.com>
2022-11-11 17:21:15 +0200

    update queue2

--- include/Queue.h
+++ include/Queue.h
@@ -1,7 +1,6 @@
 #ifndef LAB1_QUEUE_H
 #define LAB1_QUEUE_H
 
-
 template<class T>
 class Queue final {
 private:
@@ -16,42 +15,53 @@ private:
 public:
     Queue() = default;
 
-    void add(const T& value) {
-        Node *addNode = new Node;
-        addNode->value = value;
-        addNode->next = nullptr;
-
-        if(head == nullptr)
-            head = addNode;
-        else
-            curr->next = addNode;
-
-        curr = addNode;
-        m_iSize++;
-    }
-
     [[nodiscard]]
-    T front() const {
-        if(!isEmpty())
-            return head->value;
-    }
-
-    bool isEmpty() const {
-        return m_iSize == 0;
-    }
-
-    void poll() {
-        if(m_iSize != 0) {
-            Node *pollNode = head;
-            head = pollNode->next;
-            delete pollNode;
-            m_iSize--;
-        }
-    }
-
-    std::size_t size() const {
-        return m_iSize;
-    }
+    T front() const;
+    void add(const T& value);
+    bool isEmpty() const;
+    void poll();
+    std::size_t size() const;
 };
+
+template<class T>
+T Queue<T>::front() const {
+    if(!isEmpty())
+        return head->value;
+}
+
+template<class T>
+void Queue<T>::add(const T& value) {
+    Node *addNode = new Node;
+    addNode->value = value;
+    addNode->next = nullptr;
+
+    if(head == nullptr)
+        head = addNode;
+    else
+        curr->next = addNode;
+
+    curr = addNode;
+    m_iSize++;
+}
+
+template<class T>
+bool Queue<T>::isEmpty() const {
+    return m_iSize == 0;
+}
+
+template<class T>
+void Queue<T>::poll() {
+    if(m_iSize != 0) {
+        Node *pollNode = head;
+        head = pollNode->next;
+        delete pollNode;
+        m_iSize--;
+    }
+}
+
+template<class T>
+std::size_t Queue<T>::size() const {
+    return m_iSize;
+}
 
 #endif //LAB1_QUEUE_H
